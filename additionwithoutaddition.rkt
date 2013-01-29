2dc9e62398f4414934e84e15693574c98a6571ef
BATMAN <cpwest11@gmail.com>
2013-01-29 12:44:54 -0600

    i apologize that my template commit was lost
    OHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERS

---
+++ additionwithoutaddition.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname additionwithoutaddition) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+;add-to-pi
+;adds a given number to 3.14 without +
+(check-expect (add-to-pi 4) 7.14)
+
+(define (counter n num)
+  (cond 
+    [(= n 0) num]
+    [else (counter (sub1 n) (add1 num))]))
+(define (add-to-pi n)
+  (cond
+   [(number? n) (counter n 3.14)]
+   [else "THIS IS NOT A NUMBER YOU FOOL"]))

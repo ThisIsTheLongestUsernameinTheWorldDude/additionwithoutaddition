8698df5bdd0a29782f82f56dfac860e667bfb151
BATMAN <cpwest11@gmail.com>
2013-01-29 12:49:59 -0600

    RECORD TIME
    OHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERSOHGODTHESPIDERS

--- additionwithoutaddition.rkt
+++ additionwithoutaddition.rkt
@@ -3,13 +3,13 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname additionwithoutaddition) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 ;add-to-pi
 ;adds a given number to 3.14 without +
-(check-expect (add-to-pi 4) 7.14)
+(check-expect (add 4 3) 7)
 
 (define (counter n num)
   (cond 
-    [(= n 0) ...]
-    [else ...]))
-(define (add-to-pi n)
+    [(= n 0) num]
+    [else (counter (sub1 n) (add1 num))]))
+(define (add n num)
   (cond
-   [(number? n) (counter ...)]
+   [(number? n) (counter n num)]
    [else "THIS IS NOT A NUMBER YOU FOOL"]))
